eae7d93e8e47e7754a3d8bf943c0ac8c5a0ab78d
Nick <github@ngpixel.com>
2020-04-11 14:25:58 -0400

    feat: link to page button

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -44,10 +44,33 @@ import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 
 import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+import linkToPageIcon from './icons/link-to-page.svg';
 
 export default class DecoupledEditor extends DecoupledEditorBase {}
 
 /* global WIKI */
+
+class LinkToPage extends Plugin {
+	init() {
+		const editor = this.editor;
+
+		editor.ui.componentFactory.add( 'linkToPage', locale => {
+			const view = new ButtonView( locale );
+
+			view.set( {
+				label: 'Insert Link to Page',
+				icon: linkToPageIcon,
+				tooltip: true
+			} );
+
+			view.on( 'execute', () => {
+				WIKI.$emit( 'editorLinkToPage' );
+			} );
+
+			return view;
+		} );
+	}
+}
 
 class InsertAsset extends Plugin {
 	init() {
@@ -95,6 +118,7 @@ DecoupledEditor.builtinPlugins = [
 	ImageUpload,
 	InsertAsset,
 	Link,
+	LinkToPage,
 	List,
 	Indent,
 	Code,
@@ -136,6 +160,7 @@ DecoupledEditor.defaultConfig = {
 			'code',
 			'codeblock',
 			'|',
+			'linkToPage',
 			'link',
 			'blockquote',
 			'insertAsset',
